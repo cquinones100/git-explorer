07dd5ea7e6f2957329a0e96f634970824a70f5d0
Carlos Quinones <cquinones100@gmail.com>
2024-04-16 08:24:46 -0400

    Print git log

--- main.rb
+++ main.rb
@@ -8,15 +8,25 @@ require 'open3'
 extend T::Sig
 
 command = ARGV[0]
+path = ARGV[1]
+
+git = Git.open(path)
 
 case command
 when 'log'
-  Log.new.puts_log
+  Log.new(git).puts_log
 end
 
 class Log
+  extend T::Sig
+
+  sig { params(git: Git::Base).void }
+  def initialize(git)
+    @git = T.let(git, Git::Base)
+  end
+
   sig { void }
   def puts_log
-    Git.open
+    puts @git.log
   end
 end

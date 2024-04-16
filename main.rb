897cd0501709750419164a0f84a8822cf10f3e7a
Carlos Quinones <cquinones100@gmail.com>
2024-04-16 08:45:04 -0400

    Support git log

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require "rubygems"
 require "bundler/setup"
 require 'cli/ui'
 require 'open3'
+require 'git'
 
 extend T::Sig
 
@@ -12,21 +13,33 @@ path = ARGV[1]
 
 git = Git.open(path)
 
+class Log
+  extend T::Sig
+  
+  sig { params(git: Git::Base).void }
+  def initialize(git)
+    @git = T.let(git, Git::Base)
+  end
+  
+  sig { void }
+  def puts_log
+    CLI::UI::Frame.open('Git Log') do
+      CLI::UI::Prompt.ask('') do |handler|
+        @git.log.each do |commit|
+          handler.option("#{commit.to_s} - #{commit.author.name} - #{commit.date}") do
+            system("echo #{commit} | pbcopy")
+
+            "Copied to clipboard: #{commit}"
+          end
+        end
+      end
+    end
+  end
+end
+
 case command
 when 'log'
   Log.new(git).puts_log
 end
 
-class Log
-  extend T::Sig
-
-  sig { params(git: Git::Base).void }
-  def initialize(git)
-    @git = T.let(git, Git::Base)
-  end
-
-  sig { void }
-  def puts_log
-    puts @git.log
-  end
-end
+CLI::UI::StdoutRouter.enable
